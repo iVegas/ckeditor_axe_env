570246edf2e2fbce05a5fae2773e0c4ee245bdcb
tor <semkulich.m@gmail.com>
2019-07-05 18:44:51 +0300

    Introduse continious update of drupal core.

--- site-install.sh
+++ site-install.sh
@@ -3,6 +3,7 @@
 function in_container () { docker-compose exec -T php bash -c "$@"; }
 # Install or reinstall the demo site and enable ckeditor_axe module.
 in_container "composer install"
+in_container "composer update drupal/core webflo/drupal-core-require-dev 'symfony/*' --with-dependencies"
 git clone git@github.com:semkulich/ckeditor_axe.git web/core/modules/ckeditor_axe
 in_container "drush si demo_umami --account-pass=admin -y"
 in_container "drush en ckeditor_axe -y"
